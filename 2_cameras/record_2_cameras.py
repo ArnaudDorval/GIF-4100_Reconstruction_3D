388e187ba5675c863c85c44e6a66b4facc4c6bcf
Arnaud Dorval <arnaud.dorval-leblanc.1@ulaval.ca>
2021-11-13 19:25:26 -0500

    enregistrement 2 camera en parrallèle with threading

--- 2_cameras/record_2_cameras.py
+++ 2_cameras/record_2_cameras.py
@@ -1,77 +1,87 @@
 import os
 import numpy as np
 import cv2
-
-filename_0 = "video_0a.avi"
-filename_1 = "video_0b.avi"
+from multiprocessing import Process
 
 fps = 24.0
 res = '720p'
 
 # Standard Video Dimensions Sizes
-STD_DIMENSIONS =  {
+STD_DIMENSIONS = {
     "480p": (640, 480),
     "720p": (1280, 720),
     "1080p": (1920, 1080),
     "4k": (3840, 2160),
 }
 
+
 # Set resolution for the video capture
 def change_res(cap, width, height):
     cap.set(3, width)
     cap.set(4, height)
 
+
 # grab resolution dimensions and set video capture to it.
 def get_dims(cap, res='720p'):
     width, height = STD_DIMENSIONS["480p"]
     if res in STD_DIMENSIONS:
-        width,height = STD_DIMENSIONS[res]
+        width, height = STD_DIMENSIONS[res]
     ## change the current caputre device
     ## to the resulting resolution
     change_res(cap, width, height)
     return width, height
 
+
 # Video Encoding, might require additional installs
 # Types of Codes: http://www.fourcc.org/codecs.php
 VIDEO_TYPE = {
     'avi': cv2.VideoWriter_fourcc(*'XVID'),
-    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+    # 'mp4': cv2.VideoWriter_fourcc(*'H264'),
     'mp4': cv2.VideoWriter_fourcc(*'XVID'),
 }
+
 
 def get_video_type(filename):
     filename, ext = os.path.splitext(filename)
     if ext in VIDEO_TYPE:
-      return  VIDEO_TYPE[ext]
+        return VIDEO_TYPE[ext]
     return VIDEO_TYPE['avi']
 
 
-cap_0 = cv2.VideoCapture(1)
-out_0 = cv2.VideoWriter(filename_0, get_video_type(filename_0), 25, get_dims(cap_0, res))
-cap_1 = cv2.VideoCapture(2)
-out_1 = cv2.VideoWriter(filename_1, get_video_type(filename_1), 25, get_dims(cap_1, res))
+def webcam_video(p_camera):
+    cap = cv2.VideoCapture(p_camera)
+    name = ""
+    if (p_camera == 0):
+        name = 'laptop'
+    elif (p_camera == 1):
+        name = 'left'
+    else:
+        name = 'right'
 
-while(True):
-    ret0, frame0 = cap_0.read()
-    ret1, frame1 = cap_1.read()
+    filename = "video_" + name + ".avi"
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
+    out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
 
-    if (ret0):
-        out_0.write(frame0)
-        cv2.imshow('cam 0 ', frame0)
-    if (ret1):
-        out_1.write(frame1)
-        cv2.imshow('cam 1 ', frame1)
+    while (True):
+        ret, frame = cap.read()
+        if ret == True:
+            out.write(frame)
+            cv2.imshow(name, frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
-    #enregistrer vid
 
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+if __name__ == '__main__':
+    p1 = Process(target=webcam_video, args=(1,))
+    p2 = Process(target=webcam_video, args=(2,))
+    # T = Thread(target=run, args=("Ayla",))
+    p1.start()
+    p2.start()
 
-cap_0.release()
-out_0.release()
-cap_1.release()
-out_1.release()
-cv2.destroyAllWindows()
+    p1.join()
+    p2.join()
