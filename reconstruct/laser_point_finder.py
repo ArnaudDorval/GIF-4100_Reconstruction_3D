4e39ebb8066dd399e8f355f5527143d5fa1cfff9
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-11-30 21:48:36 -0500

    Finished laser point finding

--- reconstruct/laser_point_finder.py
+++ reconstruct/laser_point_finder.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import *
+from scipy.ndimage import center_of_mass
 
 RED_THRESHOLD = 200.0
 
@@ -7,5 +8,9 @@ def get_laser_mask(frame: np.ndarray) -> np.ndarray:
     return ((frame[:, :, 2] > RED_THRESHOLD) * 255).astype("uint8")
 
 
-def get_laser_2d_position(laser_mask: np.ndarray) -> Tuple[float, float]:
-    pass
+def get_laser_2d_position(laser_mask: np.ndarray) -> Optional[Tuple[float, float]]:
+    position = center_of_mass(laser_mask)
+    if np.any(np.isnan(position)):
+        return None
+    else:
+        return  position[1].astype("uint32"), position[0].astype("uint32")
--- reconstruct/main.py
+++ reconstruct/main.py
@@ -9,22 +9,36 @@ SHOW_LASER_MASK = getenv("OPTION_SHOW_LASER_MASK", "1") == "1"
 
 def main():
     for i, (left_frame, right_frame) in enumerate(get_test_frame_pairs()):
-        left_laser_mask = get_laser_mask(left_frame)
-        right_laser_mask = get_laser_mask(right_frame)
+        frames = {
+            "left": left_frame,
+            "right": right_frame
+        }
+        laser_masks = dict()
+        laser_positions = dict()
+        for camera_position in ["left", "right"]:
+            laser_masks[camera_position] = get_laser_mask(frames[camera_position])
+            laser_positions[camera_position] = get_laser_2d_position(laser_masks[camera_position])
+
         if SHOW_LASER_MASK:
+            annotated_laser_masks = dict()
+            for camera_position in ["left", "right"]:
+                annotated_laser_masks[camera_position] = cv.cvtColor(laser_masks[camera_position], cv.COLOR_GRAY2BGR)
+                if laser_positions[camera_position]:
+                    annotated_laser_masks[camera_position] = cv.circle(annotated_laser_masks[camera_position],
+                                                                       laser_positions[camera_position], 20,
+                                                                       (0, 255, 0), 10)
+
             img = np.vstack((
-                          np.hstack(
-                              (left_frame, right_frame)
-                          ),
-                          np.hstack(
-                              (cv.cvtColor(left_laser_mask, cv.COLOR_GRAY2BGR),
-                               cv.cvtColor(right_laser_mask, cv.COLOR_GRAY2BGR))
-                          ))
-                      )
-            # img = np.hstack(
-            #                   (left_frame, right_frame)
-            #               )
-            cv.imshow(f"Laser masks", cv.resize(img, (800, 500)))
+                np.hstack(
+                    (frames["left"], frames["right"])
+                ),
+                np.hstack(
+                    (annotated_laser_masks["left"],
+                     annotated_laser_masks["right"])
+                ))
+            )
+
+            cv.imshow(f"Laser masks", cv.resize(img, (1400, 800)))
             cv.waitKey(1)
 
 
