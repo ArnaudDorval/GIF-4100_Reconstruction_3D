1580cee021f3361dc48a5802d4223080a370a187
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-12-01 15:11:40 -0500

    Updated video-to-images

--- video_to_images/video_to_images.py
+++ video_to_images/video_to_images.py
@@ -6,7 +6,7 @@ import numpy as np
 from enum import Enum
 
 DATA_PATH = Path(os.getcwd()).parent / "data"
-OUTPUT_PATH = DATA_PATH / "extracted_images_noir"
+OUTPUT_PATH = DATA_PATH / "extracted_images_sac"
 MAX_SAVED_FRAMES = 20
 BLACK_THRESHOLD = 2.0
 VISIBLE_THRESHOLD = 5.0
@@ -22,33 +22,20 @@ class SynchronizationState(Enum):
 def extract_video_images(camera_position):
     os.makedirs(OUTPUT_PATH / camera_position, exist_ok=True)
 
-    video = cv.VideoCapture(str(DATA_PATH / f"video_{camera_position}_couleur_Trim.mp4"))
+    video = cv.VideoCapture(str(DATA_PATH / "videos_sac" / f"video_{camera_position}.avi"))
     video_frame_count = video.get(cv.CAP_PROP_FRAME_COUNT)
 
     success = True
     frame_number = 0
-    synchronization_frame = None
-    state = SynchronizationState.WAITING_FOR_FIRST_BLACK_FRAME
-
 
     while success and frame_number < video_frame_count:
         success, image = video.read()
 
-        # State machine for synchronizing video frame numbers
-        # i.e. find first frame with laser
-        if state == SynchronizationState.WAITING_FOR_FIRST_BLACK_FRAME:
-            if np.all(np.mean(image, (0, 1)) < BLACK_THRESHOLD):
-                state = SynchronizationState.WAITING_FOR_FIRST_LASER_FRAME
-        elif state == SynchronizationState.WAITING_FOR_FIRST_LASER_FRAME:
-            if np.all(np.mean(image, (0, 1)) > VISIBLE_THRESHOLD):
-                synchronization_frame = frame_number
-                state = SynchronizationState.RECORDING
-        else:
-            images[camera_position].append(image)
-            synchronized_frame_number = frame_number - synchronization_frame
-            filename = f"frame_{camera_position}_{synchronized_frame_number:05}.png"
-            # cv.imwrite(str(OUTPUT_PATH / camera_position / filename), image)
-            print(f"Wrote image {filename}.")
+        images[camera_position].append(image)
+        synchronized_frame_number = frame_number
+        filename = f"frame_{camera_position}_{synchronized_frame_number:05}.png"
+        cv.imwrite(str(OUTPUT_PATH / camera_position / filename), image)
+        print(f"Wrote image {filename}.")
 
         frame_number += 1
 
